6fc9458d7687f1aa90be82e55d0b5644d55cca5c
Yurchenko-Anton <classroomantonyurchenko@gmail.com>
2022-10-19 15:15:28 +0300

    add Parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import checkCorrectFileFormat.CheckCorrectFileFormat;
+import exception.DirectoryIsEmpty;
+import exception.WrongFormatException;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, DirectoryIsEmpty, WrongFormatException {
+        new CheckCorrectFileFormat().CheckFileFormat("data");
     }
 }
